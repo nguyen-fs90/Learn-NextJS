9b1da740ff5887f7afe60fc98814158fdaa36890
Nguyễn Mạnh Hiệp <manhhiep@MacBook-Pro-cua-Nguyen.local>
2020-12-06 21:50:35 +0900

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
-const { execOnce } = require("next/dist/next-server/lib/utils")
-
-/**
- * Index page
- */
-const Home = () => {
-    return <h1>hello world</h1>
+const Home = (props) => {
+    return <h1>{props.name}</h1>
 }
+
+export const getStaticProps = async () => {
+    return {
+        props: {
+            name: 'Nguyen Manh Hiep',
+        }
+    }
+};
+
 export default Home
